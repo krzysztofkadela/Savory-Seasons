d743da46392f08d66c83adefb84c89a28b834087
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-10-24 18:49:48 +0000

    Initial commit.

---
+++ savory_seasons/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for savory_seasons project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'savory_seasons.settings')
+
+application = get_asgi_application()
