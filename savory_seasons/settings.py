0dc679c0fa3f0ba7a184758b22cc1d6400278343
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-11-01 21:27:27 +0000

    Fix: settings.py missing comas

--- savory_seasons/settings.py
+++ savory_seasons/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant_main',
-    'menu'
+    'menu',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'savory_seasons.urls'
